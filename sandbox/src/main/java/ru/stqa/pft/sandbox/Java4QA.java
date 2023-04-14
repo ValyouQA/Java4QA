2230f2343e901278df49f5ff5111f2ad7af23e53
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-04-15 02:12:46 +0300

    add classes and objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Java4QA.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Java4QA.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class Java4QA {
 
   public static void main(String[] args) {
@@ -7,23 +9,22 @@ public class Java4QA {
     hello("user");
     hello("Alexey");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
   }
 
   public static void hello (String somebody) {
     System.out.println ("Hello, " + somebody + "!");
   }
 
-  public static double area (double len) {
-    return len * len;
+  public static double area (Square s) {
+    return s.l * s.l;
   }
 
-  public static double area (double a, double b) {
-    return a * b;
+  public static double area (Rectangle r) {
+    return r.a * r.b;
   }
 }
