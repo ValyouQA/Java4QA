138a7de140c1187496418b4a592f509d0a4f86c8
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-04-15 03:08:38 +0300

    task 2 subtask 4 add method to check distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -2,16 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class Distance {
   public static void main (String[] args) {
-    Point p1 = new Point();
-    p1.x = 3;
-    p1.y = 5;
-    Point p2 = new Point();
-    p2.x = 3;
-    p2.y = 7;
-    System.out.println("Расстояние между точками = " + distance(p1, p2));
-  }
-  public static double distance (Point p1, Point p2) {
-    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y- p2.y) * (p1.y- p2.y));
+    Point p1 = new Point(4, 5);
+    Point p2 = new Point(4, 7);
+    System.out.println("Расстояние между точками = " + p1.distance(p2));
   }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,4 +3,13 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance (Point p2) {
+    return Math.sqrt((x - p2.x) * (x - p2.x) + (y- p2.y) * (y- p2.y));
+  }
 }
