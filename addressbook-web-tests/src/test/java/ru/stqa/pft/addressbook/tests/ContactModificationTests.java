eec53e9dd5a73dc9c34b3ca75d752637b156564d
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-04-16 17:55:13 +0300

    add ContactModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.models.ContactData;
+
+public class ContactModificationTests extends TestBase{
+
+  @Test
+  public void testContactModification() {
+    app.getContactHelper().initContactModification ();
+    app.getContactHelper().fillContactForm(new ContactData("Saveliy", "Petrovich", "Petrov", "Petrovich home place", "+73988767575", "Petrovich@mail.ru", "20", "August", "1910"));
+    app.getContactHelper().submitContactModification ();
+  }
+}
