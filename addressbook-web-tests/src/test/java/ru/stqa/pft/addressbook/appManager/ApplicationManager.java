c539b871dab06a7350cb2efdc8ecb2f8fc40ff3b
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-04-23 00:25:16 +0300

    Added a condition to start the browser

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ApplicationManager.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.addressbook.appManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,20 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    if (browser.equals(Browser.FIREFOX.browserName())) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(Browser.CHROME.browserName())) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(Browser.IE.browserName())) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
