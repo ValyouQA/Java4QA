8bfdffc2241845769d5fbb9784ce2faf27beac10
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-05-01 01:08:13 +0300

    added List sizes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/GroupHelper.java
@@ -2,8 +2,12 @@ package ru.stqa.pft.addressbook.appManager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.models.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -33,8 +37,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup() {
-    click(By.name("selected[]"));
+  public void selectGroup(int index) {
+    wd.findElements(By.name("selected[]")).get(index).click();
   }
 
   public void initGroupModification() {
@@ -48,13 +52,24 @@ public class GroupHelper extends HelperBase {
   public void createGroup(GroupData group) {
     NavigationHelper nh = new NavigationHelper(wd);
     nh.goToGroupPage();
-    int before = getGroupCount();
+    List<GroupData> before = getGroupList();
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
     returnToGroupPage();
-    int after = getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    List<GroupData> after = getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements){
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 
   public int getGroupCount() {
