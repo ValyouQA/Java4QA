592ee4e76e1bfd73b8db04d3a82238b02cbc1571
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-06-13 04:10:08 +0300

    task 9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
@@ -83,7 +83,8 @@ public class ContactHelper extends HelperBase{
     List<WebElement> elements = wd.findElements(By.cssSelector("tr[name='entry']"));
     for (WebElement element : elements) {
       String name = element.getText();
-      ContactData contact = new ContactData(name,  null, null, null, null, null, null, null, null, null);
+      String lastname = element.getText();
+      ContactData contact = new ContactData(name,  null, lastname, null, null, null, null, null, null, null);
     contacts.add(contact);
     }
     return contacts;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/ContactData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.models;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String name;
   private final String middlename;
@@ -63,5 +65,26 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(name, that.name) && Objects.equals(lastname, that.lastname);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, lastname);
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "name='" + name + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
   }
 }
