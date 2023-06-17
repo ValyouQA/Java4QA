4e879650db57818373f8143a04ab16c9baccc44d
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-06-17 10:38:15 +0300

    Hash & cache

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
@@ -68,12 +68,14 @@ public class ContactHelper extends HelperBase{
     nh.addContactPage();
     fillContactForm(contact, true);
     submitContactCreation();
+    contactCache = null;
     nh.homePage();
   }
 
   public void delete(ContactData Contact) {
     selectContactById(Contact.getId());
     deleteSelectedContact();
+    contactCache = null;
     NavigationHelper nh = new NavigationHelper(wd);
     nh.homePage();
   }
@@ -82,19 +84,30 @@ public class ContactHelper extends HelperBase{
     initContactModification(contact.getId());
     fillContactForm(contact, false);
     submitContactModification();
+    contactCache = null;
     NavigationHelper nh = new NavigationHelper(wd);
     nh.homePage();
   }
 
+  private Contacts contactCache = null;
+
+
   public Contacts all() {
-    Contacts contacts = new Contacts();
+    if (contactCache != null){
+      return new Contacts(contactCache);
+    }
+    contactCache = new Contacts();
     List<WebElement> elements = wd.findElements(By.xpath("//tr[@name='entry']"));
     for (WebElement element : elements) {
       String firstname = element.findElement(By.xpath(".//td[3]")).getText();
       String lastname = element.findElement(By.xpath(".//td[2]")).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
-      contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+      contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
     }
-    return contacts;
+    return new Contacts(contactCache);
+  }
+
+  public int count() {
+    return wd.findElements(By.name("selected[]")).size();
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -24,9 +24,8 @@ public class ContactModificationTests extends TestBase{
     ContactData modifiedContact = before.iterator().next();
     ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstname("Albert").withLastname("Azazoev").withHomePhoneNumber("+75535140854").withEmail("Albert@mail.ru");
     app.contact().modify(contact);
+    assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size()));
-
     assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
   }
 }
