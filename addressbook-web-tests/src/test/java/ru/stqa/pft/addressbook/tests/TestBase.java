836f5132841e55538e15524be566001ede26769b
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-06-19 02:54:31 +0300

    Added logging

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appManager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
@@ -18,6 +26,14 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+  @BeforeMethod (alwaysRun = true)
+  public void logTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop test " + m.getName());
   }
 
 }
