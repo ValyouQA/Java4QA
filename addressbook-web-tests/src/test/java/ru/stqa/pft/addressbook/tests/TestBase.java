8562bd9190939ccc48b2a2aa2d4f77e8294ed3b9
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-06-19 02:29:22 +0300

    Transferring the description of the test stand to the configuration file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,8 @@ import ru.stqa.pft.addressbook.appManager.ApplicationManager;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
+  protected static ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
