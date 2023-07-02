22988f161ec40be5d77fdbca461c6ecb406756bc
Valeriy Yudin <valeriy.yudnin@gmail.com>
2023-07-02 22:28:22 +0300

    fıx

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -4,7 +4,7 @@ public class Distance {
   public static void main (String[] args) {
     Point p1 = new Point(4, 5);
     Point p2 = new Point(4, 7);
-    System.out.println("Расстояние между точками = " + p1.distance(p2));
+    System.out.println("Distance between points = " + p1.distance(p2));
   }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Java4QA.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Java4QA.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.sandbox;
 
-import java.awt.*;
-
 public class Java4QA {
 
   public static void main(String[] args) {
@@ -10,10 +8,10 @@ public class Java4QA {
     hello("Alexey");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    System.out.println("Area of a square with a side " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Area of a rectangle with sides " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
